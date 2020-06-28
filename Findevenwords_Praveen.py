7b5eee06890c260f9b91daf1ac19b9bcf90857b2
princearora07 <hadoopforbigdata@gmail.com>
2020-06-28 13:28:53 +0530

    JIRA 567

--- Findevenwords_Praveen.py
+++ Findevenwords_Praveen.py
@@ -1,8 +1,9 @@
 print("This program is to find even words in the given sentence")
 st_2 = 'Print every word in this sentence that has an even number of letters'
-spl = st_2.split(' ')
+print("Program ended....")
 for word in spl:
     if len(word) % 2 == 0:
         print("even!: ", 'The word in the sentence:', word, '. The length of word:', len(word))
 
+print("Program ended....")
 print("Program ended....")
