14eed8689f705dd9ba99d031ea50709104c2b4b2
princearora07 <hadoopforbigdata@gmail.com>
2020-06-28 12:48:56 +0530

    JIRA 567

--- Findevenwords_Praveen.py
+++ Findevenwords_Praveen.py
@@ -3,3 +3,5 @@ spl = st_2.split(' ')
 for word in spl:
     if len(word) % 2 == 0:
         print("even!: ", 'The word in the sentence:', word, '. The length of word:', len(word))
+
+print("Program ended....")
