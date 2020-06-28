c3ff73ac5a08cde0356b308ed41eee31aa6d1c8d
praveenmpm88 <praveenmpm88@users.noreply.github.com>
2020-06-28 12:36:30 +0530

    XXX1234-Findevenwords

---
+++ Findevenwords_Praveen.py
@@ -0,0 +1,5 @@
+st_2 = 'Print every word in this sentence that has an even number of letters'
+spl = st_2.split(' ')
+for word in spl:
+    if len(word) % 2 == 0:
+        print("even!: ", 'The word in the sentence:', word, '. The length of word:', len(word))
